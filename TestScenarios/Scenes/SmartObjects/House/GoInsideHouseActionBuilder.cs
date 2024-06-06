53838456dcf6a01aad86dfe4f03cc7f15f1f7381
erik <enag2112@gmail.com>
2024-06-06 13:44:57 +0200

    Add house logic

---
+++ TestScenarios/Scenes/SmartObjects/House/GoInsideHouseActionBuilder.cs
@@ -0,0 +1,22 @@
+using UGOAP.Agent;
+using UGOAP.BehaviourSystem.Actions;
+using UGOAP.CommonUtils.FastName;
+using UGOAP.KnowledgeRepresentation.BeliefSystem;
+using UGOAP.KnowledgeRepresentation.Facts;
+using UGOAP.SmartObjects;
+
+public class GoInsideHouseActionBuilder : IActionBuilder
+{
+    private readonly SmartHouse _smartHouse;
+    public GoInsideHouseActionBuilder(ISmartObject smartObject) => _smartHouse = smartObject as SmartHouse;
+
+    public IAction Build(IAgent agent)
+    {
+        var action = new BasicAction.Builder(new FastName("GoInsideHouse"), agent, _smartHouse)
+            .WithCost(() => agent.Location.DistanceTo(_smartHouse.Location))
+            .WithActionLogic(new GoInsideHouseActionLogic(_smartHouse, agent))
+            .WithStateEffect(new Belief.BeliefBuilder(Facts.Predicates.IsCovered).WithCondition(() => true).Build())
+            .Build();
+        return action;
+    }
+}
---
+++ TestScenarios/Scenes/SmartObjects/House/SmartHouse.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+using UGOAP.Agent;
+using UGOAP.BehaviourSystem.Actions;
+using UGOAP.CommonUtils.FastName;
+using UGOAP.KnowledgeRepresentation.BeliefSystem;
+using UGOAP.KnowledgeRepresentation.Facts;
+using UGOAP.SmartObjects;
+
+
+[GlobalClass]
+public partial class SmartHouse : Area2D, ISmartObject
+{
+    public FastName Id { get; private set; }
+
+    public Vector2 Location => GlobalPosition;
+
+    public HashSet<IActionBuilder> SuppliedActionBuilders { get; } = new HashSet<IActionBuilder>();
+
+    public override void _Ready()
+    {
+        Id = new FastName(Name);
+        BodyEntered += OnBodyEntered;
+        BodyExited += OnBodyExited;
+        SuppliedActionBuilders.Add(new GoInsideHouseActionBuilder(this));
+        SmartObjectBlackboard.Instance.RegisterObject(Id, this);
+    }
+
+    private void OnBodyExited(Node2D body)
+    {
+        if (body is IAgent agent)
+        {
+            agent.State.BeliefComponent.UpdateBelief(new Belief.BeliefBuilder(Facts.Predicates.IsCovered).WithCondition(() => false).Build());
+        }
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+        if (body is IAgent agent)
+        {
+            agent.State.BeliefComponent.UpdateBelief(new Belief.BeliefBuilder(Facts.Predicates.IsCovered).WithCondition(() => true).Build());
+        }
+    }
+}
+
